009fc5fb6c456bcef0e960cbe2dbf6e20fc8b3e5
Sourabh037 <bhandarisourabh2003@gmail.com>
2026-02-12 18:19:02 +0530

    Initial python practice files

---
+++ VSCODE.py
@@ -0,0 +1,3 @@
+# to make a line up and down without copypaste...alt + up key or down key
+# to change same word in multiline...alt + curser
+# to drag the terminal up press on powershell/code..etc and drag it help of mouse
