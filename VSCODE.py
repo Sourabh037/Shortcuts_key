604ade851be2ddcc64e674eee253efa533a74780
Sourabh037 <bhandarisourabh2003@gmail.com>
2026-02-12 19:19:53 +0530

    changes

--- VSCODE.py
+++ VSCODE.py
@@ -1,3 +1,5 @@
 # to make a line up and down without copypaste...alt + up key or down key
 # to change same word in multiline...alt + curser
 # to drag the terminal up press on powershell/code..etc and drag it help of mouse
+# alt + shift + down duplicate line
+# ctrl + right left jump faster
